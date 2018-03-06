6eef70054f534f500eb10ad9b32c8bd305da02b1
guney bilen <guneybilen@yahoo.com>
2018-03-07 01:17:51 +0300

    end of video 91

--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -1,14 +1,17 @@
+
 const graphql = require('graphql');
 
 const {
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLID
 } = graphql;
 
 const UserType = new GraphQLObjectType({
 
   name: 'UserType',
   fields: {
+    id: { type: GraphQLID },
     email: { type: GraphQLString }
   }
 });
